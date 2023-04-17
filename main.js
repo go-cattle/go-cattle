a027e62f37aa2f7b24f459bc67d8792ec74fa576
Rellekk-Z <52284479+Sarthak-Sidhant@users.noreply.github.com>
2023-04-17 18:09:31 +0530

    version 3 im just fucked

--- main.js
+++ main.js
@@ -1,75 +1,90 @@
-// Function to read data from file
-function readDataFromFile(fileData) {
-    var data = [];
-    var lines = fileData.split('\n');
-    var i = 0;
-    while (i < lines.length) {
-        if (lines[i].startsWith('Disease:')) {
-            var disease = lines[i].trim().split(': ')[1];
-            i++;
-            var symptoms = lines[i].trim().split(': ')[1];
-            i++;
-            var remedies = lines[i].trim().split(': ')[1];
-            data.push({'disease': disease, 'symptoms': symptoms, 'remedies': remedies});
+// Read example.txt file using Fetch API
+fetch('example.txt')
+  .then(response => response.text())
+  .then(text => {
+    // Split text by lines
+    const lines = text.split('\n');
+    let diseases = [];
+    let symptoms = [];
+    let remedies = [];
+
+    // Loop through lines and extract disease, symptoms, and remedies
+    for (let i = 0; i < lines.length; i++) {
+      const line = lines[i].trim();
+      if (line.startsWith('Disease:')) {
+        diseases.push(line.replace('Disease:', '').trim());
+      } else if (line.startsWith('Symptoms:')) {
+        const symptomList = line.replace('Symptoms:', '').trim();
+        const symptomArr = symptomList.split(',');
+        for (let j = 0; j < symptomArr.length; j++) {
+          const symptom = symptomArr[j].trim();
+          if (!symptoms.includes(symptom)) {
+            symptoms.push(symptom);
+          }
         }
-        i++;
+      } else if (line.startsWith('Remedy:')) {
+        remedies.push(line.replace('Remedy:', '').trim());
+      }
     }
-    return data;
-}
 
-// Function to find remedies for a given disease
-function findRemedies(disease, data) {
-    for (var i = 0; i < data.length; i++) {
-        if (data[i]['disease'].toLowerCase().trim() === disease.toLowerCase().trim()) {
-            return data[i]['remedies'];
+    // Create checkboxes for symptoms
+    const checkboxesContainer = document.getElementById('checkboxes-container');
+    for (let i = 0; i < symptoms.length; i++) {
+      const symptom = symptoms[i];
+      const checkbox = document.createElement('input');
+      checkbox.type = 'checkbox';
+      checkbox.id = 'checkbox-' + i;
+      checkbox.value = symptom;
+      const label = document.createElement('label');
+      label.textContent = symptom;
+      label.setAttribute('for', 'checkbox-' + i);
+      checkboxesContainer.appendChild(checkbox);
+      checkboxesContainer.appendChild(label);
+      checkboxesContainer.appendChild(document.createElement('br'));
+    }
+
+    // Handle form submission
+    const form = document.getElementById('form');
+    form.addEventListener('submit', function(event) {
+      event.preventDefault();
+      const selectedSymptoms = Array.from(form.elements)
+        .filter(element => element.type === 'checkbox' && element.checked)
+        .map(element => element.value);
+
+      // Find matching disease and display remedy
+      let matchingDisease = '';
+      let matchingRemedy = '';
+      for (let i = 0; i < diseases.length; i++) {
+        const disease = diseases[i];
+        const diseaseSymptoms = lines[lines.indexOf('Disease: ' + disease) + 1]
+          .replace('Symptoms:', '')
+          .trim()
+          .split(', ')
+          .map(symptom => symptom.trim());
+        if (selectedSymptoms.every(symptom => diseaseSymptoms.includes(symptom))) {
+          matchingDisease = disease;
+          matchingRemedy = lines[lines.indexOf('Disease: ' + disease) + 2].replace('Remedy:', '').trim();
+          break;
         }
-    }
-    return null;
-}
+      }
 
-// Read data from file
-var fileData = `Disease: Disease1
-Symptoms: Symptom1, Symptom2, Symptom3
-Remedies: Remedy1, Remedy2
-
-Disease: Disease2
-Symptoms: Symptom2, Symptom3, Symptom4
-Remedies: Remedy3, Remedy4`;
-
-var data = readDataFromFile(fileData);
-
-// User input for symptoms
-var userInput = prompt("Enter the symptoms separated by commas: ");
-var symptoms = userInput.split(',').map(function(symptom) {
-    return symptom.trim().toLowerCase();
-});
-
-// Find matching diseases
-var matchingDiseases = [];
-for (var i = 0; i < data.length; i++) {
-    var itemSymptoms = data[i]['symptoms'].split(',').map(function(s) {
-        return s.trim().toLowerCase();
+      // Display matching disease and remedy
+      const resultContainer = document.getElementById('result-container');
+      resultContainer.textContent = '';
+      if (matchingDisease !== '') {
+        const diseaseHeading = document.createElement('h3');
+        diseaseHeading.textContent = 'Matching Disease: ' + matchingDisease;
+        resultContainer.appendChild(diseaseHeading);
+        const remedyParagraph = document.createElement('p');
+        remedyParagraph.textContent = 'Remedy: ' + matchingRemedy;
+        resultContainer.appendChild(remedyParagraph);
+      } else {
+        const noMatchParagraph = document.createElement('p');
+        noMatchParagraph.textContent = 'No matching disease found.';
+        resultContainer.appendChild(noMatchParagraph);
+      }
     });
-    if (itemSymptoms.some(function(symptom) {
-        return symptoms.includes(symptom);
-    })) {
-        matchingDiseases.push(data[i]['disease']);
-    }
-}
-
-if (matchingDiseases.length > 0) {
-    console.log("Matching diseases:");
-    for (var i = 0; i < matchingDiseases.length; i++) {
-        console.log("- " + matchingDiseases[i]);
-    }
-    var userInput = prompt("Enter the name of the disease for which you want to find remedies: ");
-    var remedies = findRemedies(userInput, data);
-    if (remedies) {
-        console.log("Remedies for " + userInput + ":");
-        console.log("- " + remedies.split(',').join('\n- '));
-    } else {
-        console.log("Disease not found.");
-    }
-} else {
-    console.log("No matching diseases found.");
-}
+  })
+  .catch(error => {
+    console.error('Failed to fetch example.txt:', error);
+  });
