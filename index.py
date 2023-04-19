224928cf8ddfeff99540b0a549c371cd0515cb63
Sarthak-Sidhant <sarthaksidhantisopolice@gmail.com>
2023-04-19 23:57:08 +0530

    data load go brr

--- index.py
+++ index.py
@@ -1,3 +1,9 @@
 import pandas as pd
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
+
+# Load data from file
+df = pd.read_csv("examples.txt", delimiter=":", header=None, names=["label", "text"])
+
+# Clean data
+df["text"] = df["text"].str.lower().str.strip()
