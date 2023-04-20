be18d95bac31adc03470d1f33c22bec66552c205
Sarthak-Sidhant <sarthaksidhantisopolice@gmail.com>
2023-04-20 23:42:40 +0530

    bro misspelled :skull:

--- index.py
+++ index.py
@@ -2,7 +2,7 @@ import pandas as pd
 import tensorflow as tf
 
 # Load data from file
-df = pd.read_csv("examples.txt", delimiter=":", header=None, names=["label", "text"])
+df = pd.read_csv("example.txt", delimiter=":", header=None, names=["label", "text"])
 
 # Clean data
 df["text"] = df["text"].str.lower().str.strip()
