8ff2786b3e65f1cdd9b774ce464274d1e9bfc623
Sarthak-Sidhant <sarthaksidhantisopolice@gmail.com>
2023-04-20 23:39:39 +0530

    tokenizer go brr

--- index.py
+++ index.py
@@ -10,3 +10,13 @@ df["text"] = df["text"].str.lower().str.strip()
 # Convert labels to one-hot encoding
 labels = pd.get_dummies(df["label"])
 
+# Tokenize text
+tokenizer = tf.keras.preprocessing.text.Tokenizer()
+tokenizer.fit_on_texts(df["text"])
+sequences = tokenizer.texts_to_sequences(df["text"])
+word_index = tokenizer.word_index
+
+# Pad sequences to a fixed length
+max_length = 100
+padded_sequences = tf.keras.preprocessing.sequence.pad_sequences(sequences, maxlen=max_length, padding="post")
+
